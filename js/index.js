f3c178fab2164a0a64b2144d2dc063b7c049bdd3
DevBaddy <68301104+DevBaddy@users.noreply.github.com>
2021-11-19 14:06:05 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,51 @@
+import { LEVEL, OBJECT_TYPE } from "./setup";
+
+// Classes
+import GameBoard from "./GameBoard";
+import Pacman from "./Pacman";
+
+// Dom Elements
+const gameGrid = document.querySelector("#game");
+const scoreTable = document.querySelector("#score");
+const startButton = document.querySelector("#start-button");
+
+// Game Constants
+const POWER_PILL_TIME = 10000; // ms
+const GLOBAL_SPEED = 80;
+const gameBoard = GameBoard.createGameBoard(gameGrid, LEVEL);
+
+// Initial Setup
+let score = 0;
+let timer = null;
+let gameWin = false;
+let powerPillActive = false;
+let powerPillTimer = null;
+
+function gameOver(pacman, grid) {}
+
+function checkCollision(pacman, ghosts) {}
+
+function gameLoop(pacman, ghosts) {
+  gameBoard.moveCharacter(pacman);
+}
+
+function startGame() {
+  gameWin = false;
+  powerPillActive = false;
+  score = 0;
+
+  startButton.classList.add("hide");
+
+  gameBoard.createGrid(LEVEL);
+
+  const pacman = new Pacman(2, 287);
+  gameBoard.addObject(287, [OBJECT_TYPE.PACMAN]);
+  document.addEventListener("keydown", (e) => {
+    pacman.handleKeyInput(e, gameBoard.objectExist);
+  });
+
+  timer = setInterval(() => gameLoop(pacman), GLOBAL_SPEED);
+}
+
+// Initialize game
+startButton.addEventListener("click", startGame);
