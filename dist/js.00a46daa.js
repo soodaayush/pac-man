7cf1cfaa89dbff07649ea96f8e8de921fa412884
soodaayush <aayush.sood@icloud.com>
2024-07-18 13:12:29 -0300

    Added favicon

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -704,7 +704,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50689" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64050" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
