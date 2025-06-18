34472e4aebf1e8d98d9de42c67601f4d25e82180
soodaayush <aayush.sood@icloud.com>
2025-06-18 08:17:44 -0300

    Updated packages

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -704,7 +704,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54069" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49407" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
